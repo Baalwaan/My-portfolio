62f97dbe61bf5035bb18218ac2e94e4e6db72886
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-07-13 13:03:20 +0100

    create page components

---
+++ src/components/pages/about/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.section`
+  height: 100vh;
+  background-color: pink;
+`;
+
+const AboutPage = () => <Container>About page</Container>;
+
+export default AboutPage;
