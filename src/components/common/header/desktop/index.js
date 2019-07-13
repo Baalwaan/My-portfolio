cce6302e4eaaea6085c51db5aec586ff094fd1d3
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-07-13 12:15:28 +0100

    add mediaquery to DesktopHeader

--- src/components/common/header/desktop/index.js
+++ src/components/common/header/desktop/index.js
@@ -3,10 +3,8 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 const Header = styled.nav`
-  //   display: none;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
+  display: none;
+
   position: sticky;
   top: 0;
   z-index: 1;
@@ -14,6 +12,11 @@ const Header = styled.nav`
   border-bottom: black solid 1px;
   text-align: right;
   background-color: black;
+  @media (min-width: 650px) {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
 `;
 
 const Links = styled.span`
