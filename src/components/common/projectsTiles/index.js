39132a052cbfc28daedd09f02cc6529373fdb296
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-07-13 17:39:28 +0100

    create ProjectTile and fix typo in App.js

---
+++ src/components/common/projectsTiles/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// squarecircle, github profile cards , quiz app, authentication, smoothie, workout, workout, remindly, dipict, image carousel
+
+const TileContainer = styled.div`
+  max-width: 20em;
+  min-height: 17em;
+  background: black;
+`;
+
+const Tiles = () => <TileContainer />;
+
+export default Tiles;
+
+// <div class="projects-container">
+// <div class=" projects" id="square-circle-app">
+//   <a class="project-link" href="https://github.com/FAC-Sixteen/square-circle">Square-Circle</a>
+// </div>
--- src/components/pages/projects/index.js
+++ src/components/pages/projects/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Tiles from '../../common/projectsTiles';
+
 const Container = styled.section`
-  height: 100vh;
+  min-height: 100vh;
   background-color: blue;
 `;
 
-const ProjectsPage = () => <Container>Projects page</Container>;
+const ProjectsPage = () => (
+  <Container>
+    Projects page
+    <Tiles />
+  </Container>
+);
 
 export default ProjectsPage;
