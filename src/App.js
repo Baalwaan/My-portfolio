a20e553e3c9c475a4533453bfe59c21c127de16a
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-07-13 12:50:13 +0100

    create page components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './App.css';
 // import { Router } from 'react-router-dom';
 
+//Common Components
 import Header from './components/common/header';
 
+//Pages
+import HomePage from './components/pages/home';
+import ProjectPage from './components/pages/projects';
+import AboutPage from './components/pages/about';
+import ContactPage from './components/pages/contact';
 const App = () => {
   return (
     <>
       <Header />
+      <HomePage />
+      <AboutPage />
+      <ProjectPage />
+      <ContactPage />
     </>
   );
 };
