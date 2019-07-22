a20b40d781356f61191a24b27144b66b57386d0a
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-07-22 17:56:20 +0100

    test netlify function using sendmail module to send email

--- src/components/pages/contact/index.js
+++ src/components/pages/contact/index.js
@@ -53,42 +53,48 @@ const Button = styled.button`
   color: white;
 `;
 
-const ContactPage = () => (
-  <Page>
-    <PageTitle>Get in Touch...</PageTitle>
-    <Form>
-      <InputField
-        type="text"
-        placeholder="Your name *"
-        id="name"
-        pattern="^(?=.{1,40}$)[a-zA-Z]+(?:['_.\s][a-z]+)*$"
-        name="name"
-        required
-      />
-      <InputField
-        type="email"
-        placeholder="Your email *"
-        id="email"
-        name="email"
-        pattern="[-.\w]+@([\w-]+\.)+[\w-]{2,20}"
-        required
-      />
-      <InputField
-        type="text"
-        placeholder="Your phone *"
-        class="form-data"
-        name="phone"
-        required
-      />
-      <TextArea
-        cols="30"
-        rows="10"
-        placeholder="Your message..."
-        name="message"
-      />
-      <Button>Send Message</Button>
-    </Form>
-  </Page>
-);
+const ContactPage = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    fetch(`.netlify/functions/email`).catch(error => console.log(error));
+  };
 
+  return (
+    <Page>
+      <PageTitle>Get in Touch...</PageTitle>
+      <Form onSubmit={handleSubmit}>
+        <InputField
+          type="text"
+          placeholder="Your name *"
+          id="name"
+          pattern="^(?=.{1,40}$)[a-zA-Z]+(?:['_.\s][a-z]+)*$"
+          name="name"
+          required
+        />
+        <InputField
+          type="email"
+          placeholder="Your email *"
+          id="email"
+          name="email"
+          pattern="[-.\w]+@([\w-]+\.)+[\w-]{2,20}"
+          required
+        />
+        <InputField
+          type="text"
+          placeholder="Your phone *"
+          class="form-data"
+          name="phone"
+          required
+        />
+        <TextArea
+          cols="30"
+          rows="10"
+          placeholder="Your message..."
+          name="message"
+        />
+        <Button>Send Message</Button>
+      </Form>
+    </Page>
+  );
+};
 export default ContactPage;
