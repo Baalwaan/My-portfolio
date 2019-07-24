0f280ee8cf08708e767df07548a954a7a84be9ac
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-07-24 15:30:29 +0100

    style success page

--- src/components/pages/contact/successful/index.js
+++ src/components/pages/contact/successful/index.js
@@ -9,20 +9,32 @@ const Container = styled.div`
   /* bring your own prefixes */
   transform: translate(-50%, -50%);
   border: solid black 0.5em;
-  width: 20em;
+  border-radius: 5%;
+  width: 15em;
   padding: 1em;
+  background: black;
 `;
 const Title = styled.h1`
   margin-top: 0;
+  color: white;
+`;
+
+const StyledLink = styled(Link)`
+  color: lightblue;
+`;
+
+const Message = styled.p`
+  color: white;
 `;
 
 const SuccessPage = props => {
   return (
     <Container>
       <Title>Message Sent.</Title>
-      <p>
-        To go back to home page <Link to="/">click here </Link>
-      </p>
+      <Message>
+        To go back to home page <br />
+        <StyledLink to="/">click here </StyledLink>
+      </Message>
     </Container>
   );
 };
