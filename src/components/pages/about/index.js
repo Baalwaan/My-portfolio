b38e006a7e865f4b67f2ccdc3950a2f1e27af8a1
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-08-17 19:36:28 +0100

    ammend about me page and change background to white

--- src/components/pages/about/index.js
+++ src/components/pages/about/index.js
@@ -3,25 +3,59 @@ import styled from 'styled-components';
 
 //image of me
 import BurhanImage from '../../../assets/me1.JPG';
+import Fac from '../../../assets/fac.jpg';
 
 // array of objects containing techstack Icons, url and info
 import stack from './stackIcons';
 
 const Container = styled.section`
-  width: 100vw;
-  min-height: 100vh;
-  background-color: pink;
-  background-image: url(${BurhanImage});
-  background-repeat: no-repeat;
-  background-size: cover;
-  box-shadow: inset 1px 2000px rgba(20, 10, 10, 0.6);
-  color: white;
+  background: white;
   display: flex;
-  justify-content: center;
   flex-direction: column;
+  justify-content: center;
+  // align-items: center;
+  // align-content: center;
+  min-height: 100vh;
+  width: 100vw;
+`;
+
+const Banner = styled.div`
+  position: relative;
+
+  background-image: url(${Fac});
+  width: 100vw;
+  height: 80vh;
+  background-size: cover;
+  color: white;
+  opacity: 1;
+  z-index: 1;
+  margin-bottom: 2em;
+`;
+
+const BannerOverlay = styled.div`
+  position: absolute;
+  z-index: 0;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  background: rgb(5, 121, 52, 0.6);
+  display: flex;
   align-items: flex-end;
 `;
 
+const BannerContent = styled.div`
+  padding: 1em;
+  width: 30em;
+  display: flex;
+  flex-direction: column;
+`;
+const P = styled.p`
+max-width: 20em;
+  font-size: 1.5em;
+  font-weight bolder;
+`;
 const Title = styled.h1`
   margin: 0.5em 0 0.5em 0;
   font-weight: bold;
@@ -43,6 +77,7 @@ const StyledP = styled.div`
 const TechStack = styled.div`
   text-align: center;
   width: 100%;
+  // background: white;
 `;
 const IconsContainer = styled.div`
   display: flex;
@@ -80,45 +115,19 @@ const IconTitle = styled.span`
 
 const AboutPage = () => (
   <Container>
-    <Bio>
-      <Title>About Me</Title>
-      <p>
-        A <b>tech curious junior full stack developer</b>, and graduate from
-        Founders & Coders. This program taught me the principles around web
-        development, exploring beyond the scope of coding.
-      </p>
-      <p>
-        I enjoyed how versatile my time was on the course, I began with a
-        mentor, and finished, giving back by planning resources, then mentoring
-        the new cohort. During the program I attended meet-ups and interesting
-        conferences, <b> something which our group continues to do</b>.
-      </p>
-      <p>
-        Being given the opportunity to work as <b>QA</b> in our team to build an
-        application for a product owner has given me {''}
-        <b>
-          client facing and team based experience of working in an agile
-          environment
-        </b>
-        , from this I am very aware of processes and understand the importance
-        of clear communication.
-      </p>
+    <Banner>
+      <BannerOverlay>
+        <BannerContent>
+          <h1>ABOUT ME</h1>
+          <P>
+            A tech curious junior full stack developer, and graduate from
+            Founders & Coders. This program taught me the principles around web
+            development, exploring beyond the scope of coding.
+          </P>
+        </BannerContent>
+      </BannerOverlay>
+    </Banner>
 
-      <p>
-        As <b>an aspiring</b> Junior Full Stack developer I am interested in
-        many aspects across the stack. The interaction between the client-side
-        and server-side made me really curious about how data is handled.
-      </p>
-      <p>
-        Nonetheless, I have a strong interest in <b>React</b> because of the way
-        it makes managing things a lot easier with features like the{' '}
-        <b>virtual DOM</b>, handling data with <b>states</b> and its{' '}
-        <b>component based structure</b>. I am also currently taking an online
-        course ‘Intro to <b>Redux</b>’ and would like to work with React in a
-        new role.
-      </p>
-      <StyledP>PS This is a React portfolio!</StyledP>
-    </Bio>
     <TechStack>
       <h2>Tech Stack</h2>
       <IconsContainer>
