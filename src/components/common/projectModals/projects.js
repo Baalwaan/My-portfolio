190264ee47479b8fa380aff9f4a0e39cafa76997
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-09-04 20:11:00 +0100

    create projects information for modals to use

---
+++ src/components/common/projectModals/projects.js
@@ -0,0 +1,52 @@
+const projects = [
+  {
+    project_name: 'Square Circle',
+    description:
+      'A platform helping to connect young people with unbiased employers.',
+    repo_link: 'https://github.com/FAC-Sixteen/square-circle',
+    live_version: 'https://github.com/FAC-Sixteen/square-circle'
+  },
+
+  {
+    project_name: 'Github Profile Cards',
+    description: 'An app that can be used to create a team of 5 github members',
+    repo_link: 'https://github.com/Baalwaan/Github-profile-cards',
+    live_version: 'https://objective-goodall-253402.netlify.com/'
+  },
+  {
+    project_name: 'How Many Stars',
+    description: 'Can you guess the number of stars before the clock runs out',
+    repo_link: 'https://github.com/Baalwaan/star-game',
+    live_version: 'https://its-star-game.netlify.com'
+  },
+  {
+    project_name: 'Remindly',
+    description:
+      'The birthday reminder app will help you never forget a loved one’s special day again. #Remindly. Don’t be that friend!',
+    repo_link: 'https://github.com/Baalwaan/Remindly',
+    live_version: 'https://github.com/Baalwaan/Remindly'
+  },
+
+  {
+    project_name: 'Authentication Project',
+    description:
+      'Curious about how sensitive information can be stored in and accessed from databases securely. So i decided to build a simple sign up and login form 😊.',
+    repo_link: 'https://github.com/Baalwaan/Authentication-By-Me',
+    live_version: 'https://github.com/Baalwaan/Remindly'
+  },
+  {
+    project_name: 'Smoothy Barr App',
+    description: 'A virtual smoothie bar! place your order at SmoothieBarr',
+    repo_link: 'https://github.com/Baalwaan/Smoothie-bar',
+    live_version: 'https://smoothiebar.herokuapp.com/'
+  },
+  {
+    project_name: 'Workout-Of-The-Day',
+    description:
+      'An app that lets a user post a form to a Postgrsql  database about their favourite workout of the day.',
+    repo_link: 'https://github.com/Baalwaan/Authentication-By-Me',
+    live_version: 'https://express-handlebars-project.herokuapp.com/'
+  }
+];
+
+export default projects;
